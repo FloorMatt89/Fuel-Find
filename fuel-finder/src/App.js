da8348066c48ce2bf433de27c85aedcbbb047578
FloorMatt <86789069+FloorMatt89@users.noreply.github.com>
2023-12-03 20:24:09 -0500

    Updated App.js

--- fuel-finder/src/App.js
+++ fuel-finder/src/App.js
@@ -1,25 +1,148 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  Flex,
+  HStack,
+  IconButton,
+  Input,
+  SkeletonText,
+  Text,
+} from '@chakra-ui/react'
+
+import { FaLocationArrow, FaTimes } from 'react-icons/fa'
+
+import{useLoadScript, GoogleMap, Marker, Autocomplete, DirectionsRenderer,} from '@react-google-maps/api'
+import { useRef, useState } from 'react'
+
+const center = {lat:29.650444, lng:-82.342986}
 
 function App() {
+  const {isLoaded} = useLoadScript({
+    /*Will be pushed to github without actual key */
+    googleMapsApiKey : 'Here is where the key should be',
+    libraries: ['places'],
+
+  })
+  
+  const [map, setmap] = useState(/**@type google.maps.Map */(null))
+  const [directionsResponse, setDirectionsResponse] = useState(null)
+  const [distance, setDistance] = useState('')
+  const [duration, setDuration] = useState('')
+
+  /**@type React.MutableRefObject<HTMLInputElement> */
+  const originRef = useRef()
+  /**@type React.MutableRefObject<HTMLInputElement> */
+  const destinationRef = useRef()
+
+  if(!isLoaded){
+    return <SkeletonText />
+  }
+  async function calculateRoute(){
+    if(originRef.current.value === '' || destinationRef.current.value ===''){
+      return
+    }
+    // eslint-disable-next-line no-undef
+    const directionsService = new google.maps.DirectionsService()
+    const results = await directionsService.route({
+      origin: originRef.current.value,
+      destination: destinationRef.current.value,
+      // eslint-disable-next-line no-undef
+      travelMode: google.maps.TravelMode.DRIVING,
+    })
+    setDirectionsResponse(results)
+    setDistance(results.routes[0].legs[0].distance.text)
+    setDuration(results.routes[0].legs[0].duration.text)
+  }
+
+  function clearRoute() {
+      setDirectionsResponse(null)
+      setDistance('')
+      setDuration('')
+      originRef.current.value = ''
+      destinationRef.current.value = ''
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Christian Minecraft Server loading...
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Flex
+      position='relative'
+      flexDirection='column'
+      alignItems='center'
+      h='100vh'
+      w='100vw'
+    >
+      <Box position='absolute' left={0} top={0} h='100%' w='100%'>
+        {/* Google Map Box*/}
+        <GoogleMap 
+            center={center} 
+            zoom={15} 
+            mapContainerStyle={{ width: '100%', height: '100%' }}
+            options={{
+              streetViewControl: false,
+               mapTypeControl: false, 
+            }}
+            onLoad={(map)=> setmap(map)}
+          >
+           
+            {/* Displaying markers or directions */}
+            <Marker position={center} />
+            {directionsResponse && (
+            <DirectionsRenderer directions={directionsResponse} />
+            )}
+          </GoogleMap>
+
+      </Box>
+
+      <Box
+        p={4}
+        borderRadius='lg'
+        m={4}
+        bgColor='white'
+        shadow='base'
+        minW='container.md'
+        zIndex='1'
+      >
+        <HStack spacing={2} justifyContent='space-between'>
+        <Box flexGrow={1}>
+            <Autocomplete>
+              <Input type='text' placeholder='Origin' ref={originRef} />
+            </Autocomplete>
+          </Box>
+          <Box flexGrow={1}>
+            <Autocomplete>
+              <Input
+                type='text'
+                placeholder='Destination'
+                ref={destinationRef}
+              />
+            </Autocomplete>
+          </Box>
+          <ButtonGroup>
+            <Button colorScheme='blue' type='submit' onClick={calculateRoute}>
+              Calculate Route
+            </Button>
+            <IconButton
+              aria-label='center back'
+              icon={<FaTimes />}
+              onClick={clearRoute}
+            />
+          </ButtonGroup>
+        </HStack>
+        <HStack spacing={4} mt={4} justifyContent='space-between'>
+          <Text>Distance: {distance}</Text>
+          <Text>Duration: {duration}</Text>
+          <IconButton
+            aria-label='center back'
+            icon={<FaLocationArrow />}
+            isRound
+            onClick={() => {
+              map.panTo(center)
+              map.setZoom(15)
+            }}
+          />
+        </HStack>
+      </Box>
+    </Flex>
+  )
 }
 
-export default App;
+export default App
