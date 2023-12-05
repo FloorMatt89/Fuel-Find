15f27985923900bff5a1f7bf686afbffd9a95657
FloorMatt <86789069+FloorMatt89@users.noreply.github.com>
2023-12-04 20:56:02 -0500

    Final app UI update

--- fuel-finder/src/App.js
+++ fuel-finder/src/App.js
@@ -24,20 +24,33 @@ import {
   MenuItem,
 } from '@chakra-ui/react'
 
+
 import { FaLocationArrow, FaTimes } from 'react-icons/fa'
+import { KDTree, getRandomCoordinate, meterToMiles, milesToMeters } from './kdtree'
+import React, { useEffect } from 'react';
 
 import{useLoadScript, GoogleMap, Marker, Autocomplete, DirectionsRenderer,} from '@react-google-maps/api'
 import { useRef, useState } from 'react'
 
 const center = {lat:29.650444, lng:-82.342986}
 
+  const coords = [];
+  for (let i=0; i < 100000; i++){
+      coords[i] = getRandomCoordinate();
+  }
+
+  //create tree of coordinates
+  const kdTree = new KDTree(coords);
+ 
+  
 function App() {
   const {isLoaded} = useLoadScript({
     /*Will be pushed to github without actual key */
-    googleMapsApiKey : 'Get you own key!',
+    googleMapsApiKey : 'AIzaSyBYDrfVmz1SfEwnkdmsnX9LxCXgv_bV1rs',
     libraries: ['places'],
 
   })
+  
 
   const [selectedAlgorithm, setSelectedAlgorithm] = useState('');
 
@@ -45,10 +58,17 @@ function App() {
     setSelectedAlgorithm(algorithm);
   };
   
+  
   const [map, setmap] = useState(/**@type google.maps.Map */(null))
   const [directionsResponse, setDirectionsResponse] = useState(null)
+  const [directionsRenderer, setDirectionsRenderer] = useState(null)
   const [distance, setDistance] = useState('')
   const [duration, setDuration] = useState('')
+  const [algorithmTime, setAlgorithmTime] = useState('');
+  const [gasStations, setGasStations] = useState([])
+  const [centerMarker, setCenterMarker] = useState(null)
+  let elapsedTime = 0;
+  
 
   /**@type React.MutableRefObject<HTMLInputElement> */
   const originRef = useRef()
@@ -59,15 +79,64 @@ function App() {
   /**@type React.MutableRefObject<HTMLInputElement> */
   const maxRangeRef = useRef()
 
+  useEffect(() => {
+    const initMap = async () => {
+      if (isLoaded) {
+        try {
+          const epolyModule = await import('./epoly.js');
+          console.log('epoly.js has been loaded');
+        } catch (error) {
+          console.error('Error loading epoly.js:', error);
+        }
+      } else {
+        setTimeout(initMap, 100);
+      }
+    };
+  
+
+    initMap();
+    return () => {
+      
+    };
+  }, [isLoaded]);
+
   if(!isLoaded){
     return <SkeletonText />
   }
-  async function calculateRoute(){
-    if(originRef.current.value === '' || destinationRef.current.value ===''){
-      return
+ 
+  
+
+  
+
+  const calculateRoute = async () => {
+    if (originRef.current.value === '' || destinationRef.current.value === '' || currentRangeRef.current.value === '' || maxRangeRef.current.value === '') {
+      return;
     }
+    const startTime = performance.now();
+    console.log('calculateRoute called');
     // eslint-disable-next-line no-undef
-    const directionsService = new google.maps.DirectionsService()
+    const directionsService = new google.maps.DirectionsService();
+    const origin = originRef.current.value;
+    const destination = destinationRef.current.value;
+    const currentRange = currentRangeRef.current.value;
+    const maxRange = maxRangeRef.current.value;
+
+    const waypoints = [];
+    await calculateSegment(origin, destination, waypoints,origin, currentRange,maxRange,true);
+    const waypointsWithNeighbors = await Promise.all(
+      waypoints.map(async (waypoint) => {
+        const nearestNeighbor = await getNearestNeighbor(waypoint.location);
+        return {
+          // eslint-disable-next-line no-undef
+          location: new google.maps.LatLng(nearestNeighbor[0], nearestNeighbor[1]),
+          stopover: true,
+        };
+      })
+    );
+  
+    displayRoute(origin, destination, waypointsWithNeighbors);
+    // eslint-disable-next-line no-undef
+    
     const results = await directionsService.route({
       origin: originRef.current.value,
       destination: destinationRef.current.value,
@@ -77,7 +146,110 @@ function App() {
     setDirectionsResponse(results)
     setDistance(results.routes[0].legs[0].distance.text)
     setDuration(results.routes[0].legs[0].duration.text)
-  }
+    const endTime = performance.now();
+    elapsedTime = endTime-startTime;
+    setAlgorithmTime(elapsedTime);
+   
+  };
+  const getNearestNeighbor = async (point) => {
+    return new Promise((resolve) => {
+      // Start measuring time
+      
+      let nearestNeighbor;
+      console.log('Neighbor called');
+      if (selectedAlgorithm === 'Nearest Neighbor-Normal') {
+        console.log('pick 1');
+        nearestNeighbor = kdTree.nearestNeighbor(point);
+
+      } else if (selectedAlgorithm === 'Nearest Neighbor-Priority Queue') {
+        console.log('pick 2');
+        nearestNeighbor = kdTree.nearestNeighborWithPriorityQueue(point);
+        
+      }
+      // Resolve with both the nearestNeighbor and the measured time
+      resolve({ nearestNeighbor});
+    });
+  };
+  
+  
+  
+  const calculateSegment = (start, end, waypoints,initialStart, currRange, maxRange, firstPass ) => {
+    // eslint-disable-next-line no-undef
+    const directionsService = new google.maps.DirectionsService();
+    console.log('calculateSegment called');
+    directionsService.route(
+      {
+        origin: start,
+        destination: end,
+        travelMode: 'DRIVING',
+      },
+       async (response, status) => {
+        if (status === 'OK') {
+          
+          // eslint-disable-next-line no-undef
+          const polyline = new google.maps.Polyline({ path: response.routes[0].overview_path });
+          let point;
+            if (firstPass) {
+                point = polyline.GetPointAtDistance(milesToMeters(currRange));
+            } else {
+                point = polyline.GetPointAtDistance(milesToMeters(maxRange));
+            }
+
+          if (point) {
+              waypoints.push({
+              location: point,
+              stopover: true,
+            });
+            // Recursively calculate the next segment
+            calculateSegment(point, end, waypoints, initialStart, maxRange, maxRange, false);
+          } else {
+            // No more points at 300 miles, display the final route
+            displayRoute(initialStart, end, waypoints);
+          }
+        } else {
+          window.alert('Directions request failed due to ' + status);
+        }
+      }
+    );
+  };
+  const displayRoute = (start, end, waypoints) => {
+    console.log('displayRoute called');
+    // eslint-disable-next-line no-undef
+    const directionsService = new google.maps.DirectionsService();
+    const origin = start;
+    const destination = end;
+
+    directionsService.route(
+      {
+        origin: origin,
+        destination: destination,
+        waypoints: waypoints,
+        travelMode: 'DRIVING',
+      },
+      (response, status) => {
+        if (status === 'OK') {
+          if (directionsRenderer) {
+            directionsRenderer.setDirections(response);
+          }
+        } else {
+          window.alert('Directions request with waypoints failed due to ' + status);
+        }
+      }
+    );
+  };
+
+  const handleMapLoad = (map) => {
+    setmap(map);
+    console.log('maploaded called');
+    // Create a new DirectionsRenderer when the map loads
+    if (!directionsRenderer) {
+      // eslint-disable-next-line no-undef
+      const newDirectionsRenderer = new google.maps.DirectionsRenderer();
+      newDirectionsRenderer.setMap(map);
+      setDirectionsRenderer(newDirectionsRenderer);
+    }
+  };
+ 
 
   function clearRoute() {
       setDirectionsResponse(null)
@@ -86,6 +258,10 @@ function App() {
       originRef.current.value = ''
       destinationRef.current.value = ''
   }
+
+ 
+
+
   return (
     <Flex
       position='relative'
@@ -104,7 +280,7 @@ function App() {
               streetViewControl: false,
                mapTypeControl: false, 
             }}
-            onLoad={(map)=> setmap(map)}
+            onLoad={(map)=> handleMapLoad(map)}
           >
            
             {/* Displaying markers or directions */}
@@ -157,10 +333,10 @@ function App() {
         
                 <Input
                   type='text'
-                  placeholder='Current Range'
+                  placeholder='Current Range(mi)'
                   ref={currentRangeRef}
                   size='sm'
-                  w='70%'
+                  w='85%'
                 />
                 
               
@@ -169,10 +345,10 @@ function App() {
             
                 <Input
                   type='text'
-                  placeholder='Max Range'
+                  placeholder='Max Rang(mi)'
                   ref={maxRangeRef}
                   size='sm'
-                  w='70%'
+                  w='85%'
                 />
             
             </Box>
@@ -197,10 +373,10 @@ function App() {
               </MenuList>
             </Menu>
             <Text mt={0.5} fontSize={14}>Selected Algorithm: {selectedAlgorithm}</Text>
-            <Text mt={0.5} fontSize={14}>Algorithm Time: </Text>
+            <Text mt={0.5} fontSize={14}>Algorithm Time:{algorithmTime} </Text>
           </VStack>
 
-          <VStack align="start" mr={150}>
+          <VStack align="start" mr = {150} >
             <Text>Distance: {distance}</Text>
             <Text>Duration: {duration}</Text>
           </VStack>
